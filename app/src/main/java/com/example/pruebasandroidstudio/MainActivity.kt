5bf0a828ac87f1867ac1f40baeca3bca448b1182
AlessioFTP <jrojas78@uan.edu.co>
2025-09-16 16:59:36 -0500

    3.4 TextCompose

--- app/src/main/java/com/example/pruebasandroidstudio/MainActivity.kt
+++ app/src/main/java/com/example/pruebasandroidstudio/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.pruebasandroidstudio.ui.theme.PruebasAndroidStudioTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +27,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PruebasAndroidStudioTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    GreetingText(
+                        name = "Angie",
+                        from ="Nicolas",
+                        modifier = Modifier.padding(8.dp))
                 }
             }
         }
@@ -31,17 +42,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hola $name!",
+fun GreetingText(name: String, from: String, modifier: Modifier = Modifier) {
+    Column (
+        verticalArrangement = Arrangement.Center,
         modifier = modifier
-    )
+    ) {
+        Text(
+            text = "Hola $name!, estás invitada a mi cumpleaños",
+            fontSize = 36.sp,
+            lineHeight = 40.sp,
+            textAlign = TextAlign.Center,
+        )
+
+        Text(
+            text = "De $from",
+            fontSize = 26.sp,
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .align(alignment = androidx.compose.ui.Alignment.End)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun AppPreview() {
     PruebasAndroidStudioTheme {
-        Greeting("Android")
+        GreetingText("Angie", "Nicolas")
     }
 }
+
+
+
+
